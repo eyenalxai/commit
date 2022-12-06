5e6fb35f0c386265a2dbe8bc4ff91f09af5a02f4
Yest Izit <hi@dmitry.email>
2022-12-06 18:55:52 +0400

    Here it is

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,6 +2,10 @@ import { NextRequest, NextResponse } from "next/server"
 import { findCommitByHash, getRandomCommit } from "../util/commit"
 
 export async function middleware(req: NextRequest): Promise<void | NextResponse> {
+  console.log("Middleware called")
+  console.log("Request: ", req)
+  console.log("Headers: ", req.headers)
+  
   const userAgent = req.headers.get("user-agent")
 
   if (userAgent && userAgent.toLowerCase().includes("curl")) {
