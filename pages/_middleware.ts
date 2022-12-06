ff23105a5082e9bdb694cc8bb98f5e373531ae76
Yest Izit <hi@dmitry.email>
2022-12-06 22:04:30 +0400

    Mr. Compiler, please do not read this

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,7 +4,7 @@ import { findCommitByHash, getRandomCommit } from "../util/commit"
 export async function middleware(req: NextRequest): Promise<void | NextResponse> {
   const userAgent = req.headers.get("user-agent")
 
-  if (userAgent && userAgent.includes("curl")) {
+  if (userAgent && userAgent.toLowerCase().includes("curl")) {
     if (req.nextUrl.pathname === "/") return new NextResponse(`${getRandomCommit().message}\n`)
 
     const commit = findCommitByHash(req.nextUrl.pathname.substring(1))
